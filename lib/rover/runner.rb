471a8412ad927faaf5495ceddb21edba6a8a215d
Sean DMR <sean@flyinggrizzly.net>
2022-01-15 21:14:18 +0000

    Move running logic into instance methods for cleaner separation

--- lib/rover/runner.rb
+++ lib/rover/runner.rb
@@ -6,46 +6,20 @@ module Rover
     attr_reader :board, :rover
 
     def self.run
-      runner = new
-
-      until runner.placed?
-        command = gets.chomp
-        command_elements = command.split(' ')
-        instruction = command_elements.first
-
-        next unless instruction == 'PLACE'
-
-        arguments = command_elements[1].split(',')
-
-        runner.place(arguments)
-      end
-
-      while true do
-        command = gets.chomp
-        command_elements = command.split(' ')
-        instruction = command_elements.first
-
-        case instruction
-        when 'PLACE'
-          arguments = command_elements[1].split(',')
-
-          runner.place(arguments)
-        when 'MOVE'
-          runner.move
-        when 'LEFT'
-          runner.left
-        when 'RIGHT'
-          runner.right
-        when 'REPORT'
-          runner.report
-        end
-      end
+      new.run
     end
 
     def initialize
       @board = Board.prepare
       @rover = Rover.new(board)
     end
+
+    def run
+      wait_for_placement_input
+      wait_for_all_inputs
+    end
+
+    private
 
     def placed?
       rover.placed?
@@ -75,6 +49,43 @@ module Rover
 
     def move
       rover.move!
+    end
+
+    def wait_for_placement_input
+      until placed?
+        command = gets.chomp
+        command_elements = command.split(' ')
+        instruction = command_elements.first
+
+        next unless instruction == 'PLACE'
+
+        arguments = command_elements[1].split(',')
+
+        place(arguments)
+      end
+    end
+
+    def wait_for_all_inputs
+      while true do
+        command = gets.chomp
+        command_elements = command.split(' ')
+        instruction = command_elements.first
+
+        case instruction
+        when 'PLACE'
+          arguments = command_elements[1].split(',')
+
+          place(arguments)
+        when 'MOVE'
+          move
+        when 'LEFT'
+          left
+        when 'RIGHT'
+          right
+        when 'REPORT'
+          report
+        end
+      end
     end
   end
 end
